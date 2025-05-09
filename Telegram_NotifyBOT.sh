ac15894e46f651e11aa6aa5489f3a1caaa8a6752
Nhu Thang Luu <thangln.22ns@vku.udn.vn>
2025-05-09 21:12:53 +0700

    Create Telegram_NotifyBOT.sh

---
+++ Telegram_NotifyBOT.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+logs="/home/nhuthang/log-snort/alert"
+initCount=0
+msg_caption="/tmp/telegram_msg_caption.txt"
+
+chat_id="id user"
+token="TOKEN BOT TELEGRAM"
+
+function sendAlert {
+    curl -s -F chat_id=$chat_id \
+        -F parse_mode=Markdown \
+        -F text="$caption" \
+        "https://api.telegram.org/bot$token/sendMessage"
+}
+
+while true; do
+    lastCount=$(wc -c "$logs" | awk '{print $1}')
+
+    if (( lastCount > initCount )); then
+        msg=$(tail -n 10 "$logs")
+
+        alert=$(echo "$msg" | grep "\[\*\*\]" | head -n 1 | cut -d']' -f2- | xargs)
+        time=$(echo "$msg" | grep -oE '^[0-9/:\-]+')
+        src=$(echo "$msg" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+.*->' | head -n1 | awk '{print $1}')
+        dst=$(echo "$msg" | grep -oE '-> [0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+' | head -n1 | cut -d' ' -f2)
+        payload=$(echo "$msg" | grep -A5 "$dst" | sed 's/^/    /')
+
+        echo -e "🚨 *SNORT ALERT DETECTED* 🚨\n" > "$msg_caption"
+        echo -e "*📅 Time:* \`$time\`" >> "$msg_caption"
+        echo -e "*⚠️ Alert:* \`$alert\`" >> "$msg_caption"
+        echo -e "\n*📦 Payload Info:*\n\`\`\`\n$payload\n\`\`\`" >> "$msg_caption"
+
+        caption=$(<"$msg_caption")
+        sendAlert
+
+        echo "✅ Alert sent at $(date)"
+        initCount=$lastCount
+        rm -f "$msg_caption"
+        sleep 1
+    fi
+
+    sleep 2
+done
